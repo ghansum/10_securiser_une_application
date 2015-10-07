1c99377d05929160f606d1a5ce13c752e88ad3db
ghansum <ghansumn@gmail.com>
2015-10-07 10:48:34 +0200

    mon commit

---
+++ suppression_collection.php
@@ -0,0 +1,31 @@
+<?php
+require('include/connexion.php');
+ session_start();
+$bSupprimer = false;
+/**
+ * Vérifie si un identifiant de collection est fourni
+ * et si celui-ci est bien un entier
+ */
+$token = $_SESSION['token'];
+$var = $_GET['jeton'];
+
+$iIdentifiant = filter_var($_GET['id'], FILTER_VALIDATE_INT); 
+if (isset($_GET['id']) && false !== $iIdentifiant) :
+	if(isset($_GET['jeton']) && !empty($_GET['jeton']) && $_GET['jeton'] == $token){
+    /**
+     * Supprime les ouvrages de la collection
+     */
+    $sRequeteSql = 'DELETE FROM ouvrage WHERE collection_id = ' . $iIdentifiant;
+    $oConnexion->query($sRequeteSql);
+
+    /**
+     * Supprime la collection
+     */
+    $sRequeteSql = 'DELETE FROM collection WHERE id = ' . $iIdentifiant;
+    $oConnexion->query($sRequeteSql);
+    $bSupprimer = true;
+	}
+endif;
+
+
+header('Location: index.php?page=collection.php&etat_suppression='. (int) $bSupprimer);
